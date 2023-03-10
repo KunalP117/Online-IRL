07555d1584c334055773fc0076a8b43ec969f098
KunalP117 <56691652+KunalP117@users.noreply.github.com>
2023-03-10 14:34:46 -0500

    updating main.m
    added parametrization details

--- main.m
+++ main.m
@@ -4,6 +4,17 @@ clc;
 
 %% A: Define utility function parametrization, window length
 
+pardim = 4; % dimensionality of parameter being estimated
+horizon = 100; % total number of time steps for 
+window = 10;
+%% Consider the Cobb-Douglas Utility - u(\beta) = \prod_i \beta_i^{\theta_i}
+parambounds = [0, 1]; % uniform parameter bounds for all coordinates of parameter \theta
+
+% sampling of probe - same dimension as pardim (FORNOW)
+probe_mean = rand(pardim,1);
+probe_covariance = eye(pardim)
+% instantiation via mvnrand(probe_mean,probe_covariance)
+
 
 
 %% B: Simulate: Get window length many samples, compute starting estimate.
